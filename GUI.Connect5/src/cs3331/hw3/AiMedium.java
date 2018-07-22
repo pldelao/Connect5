5154db80c575fd07235b30c587c3af63c756009c
perladelao <perladelao@172.20.28.61>
2018-07-21 19:26:12 -0600

    correct implementaion of AI

--- GUI.Connect5/src/cs3331/hw3/AiMedium.java
+++ GUI.Connect5/src/cs3331/hw3/AiMedium.java
@@ -48,15 +48,15 @@ public class AiMedium extends AI{
 		int [][] b= new int[board.getSize()][board.getSize()];
 		for(int i=0;i<board.getSize();i++) {
 			for( int j=0;j<board.getSize();j++) {
-				b[i][j]=count(i,j,1);
+				b[i][j]=count(i,j,1);//finds the number of connected discs
 				if(b[i][j]>max) {
-					max=b[i][j];
-					x=i;
+					max=b[i][j];//gets the max number of discs
+					x=i;//updates the coordinates
 					y=j;
 				}
 			}
 		}
-		if(max-1>=3) {
+		if(max-1>=3) {//if the user has 3 or more discs connected it places a disc
 			board.addDisc(x*(625/board.getSize()), y*(625/board.getSize()), 2);
 			return true;
 		}
@@ -76,17 +76,18 @@ public class AiMedium extends AI{
 		int [][] b= new int[board.getSize()][board.getSize()];
 		for(int i=0;i<board.getSize();i++) {
 			for( int j=0;j<board.getSize();j++) {
-				b[i][j]=count(i,j,2);
+				b[i][j]=count(i,j,2);//finds the number of connected discs
 				if(b[i][j]>max) {
-					max=b[i][j];
-					x=i;
+					max=b[i][j];//gets the max number of discs
+					x=i;//updates the coordinates
 					y=j;
 				}
 			}
 		}
+		//adds disc to board
 		board.addDisc(x*(625/board.getSize()), y*(625/board.getSize()), 2);
 	}
-	
+
 	/**
 
 	 * Counts how many discs are connected and adds one
@@ -102,17 +103,17 @@ public class AiMedium extends AI{
 		int c1=0,c2=0,c3=0,c4=0;
 
 		int[][] b=board.getBoard();
-		if(b[x][y]==1||b[x][y]==2) {
+		if(b[x][y]==1||b[x][y]==2) {//if one of 2 the position is not available
 			return 0;
 		}
 		if(b[x][y]==0) 
 		{
-			b[x][y]=p;
+			b[x][y]=p;//if 0 then the position is available
 		}
 		if(x+4<board.getSize()&&y+4<board.getSize()) {//d
 			for(int k=0;k<5;k++)
 			{
-				if(b[x+k][y+k]==p) {
+				if(b[x+k][y+k]==p) {//counts discs to the right and increases the counter
 					c1++;
 				}
 				else 
@@ -122,7 +123,7 @@ public class AiMedium extends AI{
 		if(x+4<board.getSize()&&y-4>=0) {//d
 			for(int k=0;k<5;k++)
 			{
-				if(b[x+k][y-k]==p)
+				if(b[x+k][y-k]==p)//counts discs up and increases the counter
 				{
 					c2++;
 				}
@@ -133,7 +134,7 @@ public class AiMedium extends AI{
 		if(y+4<board.getSize()) {//d
 			for(int k=0;k<5;k++)
 			{
-				if(b[x][y+k]==p) 
+				if(b[x][y+k]==p) //counts discs to the positive slope and increases the counter
 				{
 					c3++;
 				}
@@ -144,7 +145,7 @@ public class AiMedium extends AI{
 		if(x+4<board.getSize()){
 			for(int k=0;k<5;k++)
 			{
-				if(b[x+k][y]==p)
+				if(b[x+k][y]==p)//counts discs to the negative slope and increases the counter
 				{
 					c4++;
 				}
@@ -152,6 +153,7 @@ public class AiMedium extends AI{
 				{break;}
 			}
 		}
+		//finds the max value of the counters
 		int m1, m2;
 		if(c1>c2) {
 			m1=c1;
@@ -170,8 +172,8 @@ public class AiMedium extends AI{
 		{m1=m2;
 		}
 
-		b[x][y]=0;
-		return m1;
+		b[x][y]=0;//restores the board to its original
+		return m1;//sends the max number of discs connected
 	}
 	/**
 
@@ -180,13 +182,14 @@ public class AiMedium extends AI{
 	 */
 	public void randomPosition()
 	{
-		Random rand = new Random();
-		while(true) 
+		Random rand = new Random();//initializes the random object
+		while(true) //loops until it finds a position that isn't taken
 		{
-			int x=rand.nextInt(board.getSize());
-			int y=rand.nextInt(board.getSize()) ;
+			int x=rand.nextInt(board.getSize());//gets the x position
+			int y=rand.nextInt(board.getSize()) ;//gets the y position
 			if(board.addDisc(x*(625/board.getSize()), y*(625/board.getSize()), 2)==true) 
 			{
+				//adds disc and leaves the loop
 				break;
 			}
 		}
